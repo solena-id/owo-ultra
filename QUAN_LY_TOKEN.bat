b23c7095304ede6311be92b3d3f8a606524a8926
OwO Admin <admin@solena.id>
2026-08-08 20:03:21 +0700

    Fix huntbot captcha password hint detection for nickname

--- QUAN_LY_TOKEN.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-chcp 65001 >nul
-title Token Manager - OwO Pro Farmer VIP
-cd /d "%~dp0"
-core.exe --manage-tokens
-pause
--- START.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-chcp 65001 >nul
-title OwO Pro Farmer VIP
-cd /d "%~dp0"
-if exist "MO_DASHBOARD.bat" (
-    start "" "MO_DASHBOARD.bat"
-)
-core.exe
-pause
